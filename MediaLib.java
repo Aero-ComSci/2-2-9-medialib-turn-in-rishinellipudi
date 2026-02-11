1c5506c68aa7ec38ab2eaf10ce042d874cc3eb44
rishinellipudi <148394052+rishinellipudi@users.noreply.github.com>
2026-02-10 21:22:27 -0800

    Create MediaLib.java

---
+++ MediaLib.java
@@ -0,0 +1,111 @@
+/*
+ * Activity 2.2.9
+ */
+public class MediaLib
+{
+  public static String owner = "PLTW";
+  
+  private static int numEntries;
+  private static int numBooks;
+  private static int numMovies;
+  private static int numSongs;
+  
+  private Book book;
+  private Movie movie;
+  private Song song;
+  
+  public void addBook(String title, String author)
+  {
+    addBook(new Book(title, author));
+  }
+  
+  public void addMovie(String title, double duration)
+  {
+    addMovie(new Movie(title, duration));
+  }
+  
+  public void addSong(String title)
+  {
+    addSong(new Song(title));
+  }
+  
+  public void addBook(Book b)
+  {
+    if (book == null)
+    {
+      book = b;
+      numEntries++;
+      numBooks++;
+    } 
+    else
+      System.out.println("Cannot add a new book: A book already exists");
+  }
+  
+  public void addMovie(Movie m)
+  {
+    if (movie == null)
+    {
+      movie = m;
+      numEntries++;
+      numMovies++;
+    }
+    else
+      System.out.println("Cannot add a new movie: A movie already exists");
+  }
+  
+  public void addSong(Song s)
+  {
+    if (song == null)
+    {
+      song = s;
+      numEntries++;
+      numSongs++;
+    }
+    else
+      System.out.println("Cannot add a new song: A song already exists");
+  }
+
+  public static String getOwner()
+  {
+    return owner;
+  }
+
+  public static void changeOwner(String o)
+  {
+    owner = o;
+  }
+  
+  public static int getNumEntries()
+  {
+    return numEntries;
+  }
+  
+  public static int getNumBooks()
+  {
+    return numBooks;
+  }
+  
+  public static int getNumMovies()
+  {
+    return numMovies;
+  }
+  
+  public static int getNumSongs()
+  {
+    return numSongs;
+  }
+  
+  public String toString() 
+  {
+    String info = "";
+    
+    if (book != null)
+      info += "Book: " + book + "\n";
+    if (movie != null)
+      info += "Movie: " + movie + "\n";
+    if (song != null)
+      info += "Song: " + song + "\n";
+ 
+    return info;
+  }
+}
